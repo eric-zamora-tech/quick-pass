c04ac588c832a26d106fa4b728929c363a008021
eric-zamora-tech <eric.zamora.career@gmail.com>
2024-06-19 12:16:02 -0500

    Completed password generating logic

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,72 @@
-var generatePasswordButton = document.getElementById("generator");
-var passwordLengthSlider = document.getElementById("password-length-slider");
-var passwordLengthSliderLabel = document.getElementById("password-length-slider-label");
+let increasePasswordLengthButton = document.getElementById("increase-password-length-button");
+let decreasePasswordLengthButton = document.getElementById("decrease-password-length-button");
+let passwordLengthLabel = document.getElementById("password-length-label");
+let passwordLengthSlider = document.getElementById("password-length-slider");
+let includeUpperCaseLettersOption = document.getElementById("include-upper-case-letters-option");
+let includeLowerCaseLettersOption = document.getElementById("include-lower-case-letters-option");
+let includeNumbersOption = document.getElementById("include-numbers-option");
+let includeSpecialCharactersOption = document.getElementById("include-special-characters-option");
+let generatedPasswordLabel = document.getElementById("generated-password-label");
 
-var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
-var len = passwordLengthSlider.value;
+let passwordLength = 16;
+let availableCharacters = 0b1111;
+let generatedPassword = "";
 
-passwordLengthSliderLabel.innerHTML = len;
 
-function generatePassword(characters, length) {
-    var passwordField = document.getElementById("generated-password");
-    generatedPassword = "";
-    for (let i = 0; i < length; i++) {
-        var randomCharacterIndex = Math.floor(Math.random() * chars.length);
-        generatedPassword += characters[randomCharacterIndex];
+function updatePassword() {
+    generatedPassword = generatePassword(passwordLength, availableCharacters);
+    updateUI();
+}
+    
+function updateUI() {
+    generatedPasswordLabel.innerText = generatedPassword;
+    passwordLengthLabel.innerText = passwordLength;
+    passwordLengthSlider.value = passwordLength;
+    includeUpperCaseLettersOption.checked = availableCharacters & 0b1000;
+    includeLowerCaseLettersOption.checked = availableCharacters & 0b0100;
+    includeNumbersOption.checked = availableCharacters  & 0b0010;
+    includeSpecialCharactersOption.checked = availableCharacters  & 0b0001;
+}
+
+function updatePasswordLength(len) {
+    if(passwordLength < 1 || passwordLength > 100) return;
+    
+    passwordLength = len;
+    updatePassword();
+}
+
+function updateAvailableCharacters(e) {
+    availableCharacters ^= 1 << e.target.defaultValue;
+
+    if(availableCharacters == 0b0000) {
+        availableCharacters ^= 1 << e.target.defaultValue;
+        updateUI();
+        return
     }
-    passwordField.innerHTML = `${generatedPassword}`;
-    console.log("blank");
+
+    updatePassword();
 }
 
-passwordLengthSlider.oninput = function() {
-    len = passwordLengthSlider.value;
-    passwordLengthSliderLabel.innerHTML = len;
-    generatePassword(chars, len)
+function generatePassword(len, availableCharacters) {
+    let selectedCharacters = "";
+    let randomPassword = "";
+
+    if(availableCharacters & 0b1000) selectedCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if(availableCharacters & 0b0100) selectedCharacters += "abcdefghijklmnopqrstuvwxyz";
+    if(availableCharacters & 0b0010) selectedCharacters += "0123456789";
+    if(availableCharacters & 0b0001) selectedCharacters += "~`! @#$%^&*()_-+={[}]|:;'<,>.?/";
+
+    for(let i = 0; i < len; i++) {
+        let randomCharacterIndex = Math.floor(Math.random() * selectedCharacters.length);
+        randomPassword += selectedCharacters[randomCharacterIndex];
+    }
+
+    return randomPassword;
 }
-generatePasswordButton.addEventListener("click", () => generatePassword(chars, len));
+    
+updatePassword();
+
+increasePasswordLengthButton.addEventListener("click", () => {updatePasswordLength(++passwordLength)});
+decreasePasswordLengthButton.addEventListener("click", () => {updatePasswordLength(--passwordLength)});
+passwordLengthSlider.addEventListener("input", (e) => {updatePasswordLength(e.target.value)});
+document.querySelectorAll("input[type='checkbox']").forEach(cb => {cb.addEventListener("change", (e) => {updateAvailableCharacters(e)});});
