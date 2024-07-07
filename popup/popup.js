a044e04b19133fe9598a0a3cc3411bea1840ba1b
Eric Zamora <eric.zamora.career@gmail.com>
2024-07-07 13:06:27 -0500

    Fixed password strength bar

--- popup/popup.js
+++ popup/popup.js
@@ -5,13 +5,15 @@ let includeLowerCaseLettersOption = document.getElementById("include-lower-case-
 let includeNumbersOption = document.getElementById("include-numbers-option");
 let includeSpecialCharactersOption = document.getElementById("include-special-characters-option");
 let generatedPasswordLabel = document.getElementById("generated-password-label");
-// let regeneratePasswordButton = document.getElementById("regenerate-password-button");
+let regeneratePasswordButton = document.getElementById("regenerate-password-button");
 let copyToClipboardIcon = document.getElementById("copy-to-clipboard-icon");
 let toolTipText = document.getElementsByClassName("tooltip-text");
+let passwordStrengthBar = document.getElementById("password-strength-bar");
 
 let passwordLength = 16;
 let availableCharacters = 0b1111;
 let generatedPassword = "";
+let passwordStrength = 0;
 
 function copyPasswordToClipboard() {
     navigator.clipboard.writeText(generatedPasswordLabel.innerText);
@@ -30,11 +32,12 @@ function updatePassword() {
     generatedPassword = generatePassword(passwordLength, availableCharacters);
     updateUI();
 }
-    
+
 function updateUI() {
     generatedPasswordLabel.innerText = generatedPassword;
     passwordLengthLabel.innerText = passwordLength;
     passwordLengthSlider.value = passwordLength;
+    passwordStrengthBar.value = passwordStrength;
     includeUpperCaseLettersOption.checked = availableCharacters & 0b1000;
     includeLowerCaseLettersOption.checked = availableCharacters & 0b0100;
     includeNumbersOption.checked = availableCharacters  & 0b0010;
@@ -50,37 +53,44 @@ function updatePasswordLength(len) {
 
 function updateAvailableCharacters(e) {
     availableCharacters ^= 1 << e.target.defaultValue;
-
+    
     if(availableCharacters == 0b0000) {
         availableCharacters ^= 1 << e.target.defaultValue;
         updateUI();
         return
     }
-
+    
     updatePassword();
+}
+
+function calculatePasswordStrength(length, symbols) {
+    let entropy = Math.log2(Math.pow(symbols, length));
+    if(entropy <= 42) return 33;
+    else if (entropy > 42 && entropy <= 85) return 66;
+    else return 100;
 }
 
 function generatePassword(len, availableCharacters) {
     let selectedCharacters = "";
     let randomPassword = "";
-
+    
     if(availableCharacters & 0b1000) selectedCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     if(availableCharacters & 0b0100) selectedCharacters += "abcdefghijklmnopqrstuvwxyz";
     if(availableCharacters & 0b0010) selectedCharacters += "0123456789";
     if(availableCharacters & 0b0001) selectedCharacters += "!\"#$%" + String.fromCharCode(38) + "'()*+,-./:;" + String.fromCharCode(60) + "=>?@[\\]^_`{|}~";
-
+    
     for(let i = 0; i < len; i++) {
         let randomCharacterIndex = Math.floor(Math.random() * selectedCharacters.length);
         randomPassword += selectedCharacters[randomCharacterIndex];
     }
 
-    console.log(typeof(randomPassword), len, randomPassword);
+    passwordStrength = calculatePasswordStrength(len, selectedCharacters.length);
     return randomPassword;
 }
-    
+
 updatePassword();
 
-// regeneratePasswordButton.addEventListener("click", () => {updatePassword()});
+regeneratePasswordButton.addEventListener("click", () => {updatePassword()});
 passwordLengthSlider.addEventListener("input", (e) => {updatePasswordLength(e.target.value)});
 document.querySelectorAll("input[type='checkbox']").forEach(cb => {cb.addEventListener("change", (e) => {updateAvailableCharacters(e)});});
 copyToClipboardIcon.addEventListener("click", (e) => {copyPasswordToClipboard()});
